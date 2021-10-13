b7464e345aa937d90505a0eaff5b3122be580e51
Kostianyn <odessavshok@gmail.com>
2021-10-14 00:54:37 +0300

    +cardsMarkUp +theme-switch__toggle

--- src/index.js
+++ src/index.js
@@ -6,3 +6,47 @@ const Theme = {
   LIGHT: "light-theme",
   DARK: "dark-theme",
 };
+
+const menuRef = document.querySelector(".js-menu");
+
+const cardsMarkUp = createMarkUpMenu(menu);
+
+const bodyRef = document.body;
+
+const checkBox = document.querySelector(".theme-switch__toggle");
+
+menuRef.insertAdjacentHTML("beforeend", cardsMarkUp);
+
+function createMarkUpMenu(menu) {
+  return cardsTemplates(menu);
+}
+
+document.addEventListener("change", classToggle);
+
+themeDefault();
+
+if (bodyRef.classList.value === Theme.DARK) {
+  checkBox.checked = true;
+}
+
+function classToggle(e) {
+  if (e.target.checked) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+    localStorage.setItem("theme", Theme.DARK);
+    console.log("now is dark Theme");
+  } else {
+    bodyRef.classList.add(Theme.LIGHT);
+    bodyRef.classList.remove(Theme.DARK);
+    localStorage.setItem("theme", Theme.LIGHT);
+    console.log("now is light Theme");
+  }
+}
+
+function themeDefault() {
+  if (localStorage === null) {
+    bodyRef.classList.add(Theme.LIGHT);
+  } else {
+    bodyRef.classList.add(localStorage.getItem("theme"));
+  }
+}
